9dbf12e20da6a04c317c1027ca52c2ea6107ff77
praktikan <praktikan@SV-EIHU112-021.local>
2025-09-09 15:40:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zayyannavigationview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.zayyannavigationview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.zayyannavigationview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+}
